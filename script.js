c9d6794fff6a494e04ea465b53103c011b60937b
Jaime Ortiz <jxme88@gmail.com>
2020-12-28 17:25:00 -0800

    Added data to array to display when page is loaded.

---
+++ script.js
@@ -0,0 +1,29 @@
+// All book will go here.
+let myLibrary = [
+    {
+        title: 'Harry Potter and the Philosophers Stone',
+        author: 'J.K Rowling',
+        pages: 368,
+        read: true
+    },
+    {
+        title: 'Hell Divers',
+        author: 'Nicholas Sansbury Smith',
+        pages: 357,
+        read: false
+    },
+    {
+        title: 'The Tattooist of Auschwitz',
+        author: 'Heather Morris',
+        pages: 288,
+        read: false
+    },
+];
+
+function Book () {
+    // Constructor...
+}
+
+function addBookToLibrary () {
+    // will add books to my lbrary
+}
