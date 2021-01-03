3916f22d83115881224f3e9659f933483f96f082
Jaime Ortiz <jxme88@gmail.com>
2021-01-03 11:43:00 -0800

    connect form with script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //selectors
-let titleInput = document.querySelector('#title');
-let authorInput = document.querySelector('#author');
-let pagesInput = document.querySelector('#pages');
-let readStatus = document.querySelector('#readStatus');
+// let titleInput = document.querySelector('#title');
+// let authorInput = document.querySelector('#author');
+// let pagesInput = document.querySelector('#pages');
+// let readStatus = document.querySelector('#readStatus');
 let submitBtn = document.querySelector('#submitBtn');
 let library = document.querySelector('#library');
 
@@ -12,15 +12,6 @@ submitBtn.addEventListener('click', addBookToLibrary);
 // All book will go here.
 let myLibrary = [];
 
-function Book (id, title, author, pages, read) {
-    // Constructor...
-    this.id = id;
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-}
-
 // Create new ID string
 function createId() {
     return Math.floor((1 + Math.random()) * 0x10000)
@@ -28,16 +19,53 @@ function createId() {
       .substring(1);
 }
 
+class Book {
+  constructor(title, author, pages, read) {
+    this.id = createId();
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
+}
+
+
+
 function addBookToLibrary (event) {
     event.preventDefault();
-    console.log(event.target);
-    // will add books to my lbrary
-    let newId = createId();
 
-    let cardDiv = document.createElement('div');
+    let newBook = new Book(form.title.value, form.author.value, form.pages.value, form.readStatus.value);
+    console.log(newBook)
+    myLibrary.push(newBook);
+    console.log(form)
 
-
+    console.log(form)
     console.log('Book added!')
+}
+
+function createBookCard() {
+  let columnDiv = document.createElement("div");
+  columnDiv.classList.add("column");
+  let cardDiv = document.createElement("div");
+  cardDiv.classList.add("card");
+  columnDiv.appendChild(cardDiv);
+
+  let headerThree = document.createElement("h3");
+  headerThree.innerText = titleInput.value;
+
+  let pAuthor = document.createElement("p");
+  pAuthor.innerText = authorInput.value;
+
+  let pPages = document.createElement("p");
+  pPages.innerText = pagesInput.value;
+
+  let pReadStatus = document.createElement("p");
+  pReadStatus.innerText = readStatus.value;
+
+  cardDiv.appendChild(headerThree);
+  cardDiv.appendChild(pAuthor);
+  cardDiv.appendChild(pPages);
+  cardDiv.appendChild(pReadStatus);
 }
 
 function removeBookFromLibrary(id) {
