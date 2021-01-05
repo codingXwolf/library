e033546a1e00b652b7e7d0a02841172a024e32ef
Jaime Ortiz <jxme88@gmail.com>
2021-01-04 18:57:20 -0800

    Added state to Library component with hardcoded data. List is displaying

--- library-react/src/components/Library.js
+++ library-react/src/components/Library.js
@@ -1,23 +1,44 @@
 import React, { Component } from "react";
 //import Card from './Card';
 
-
 class Library extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      library: [
+        {
+          title: "Harry Potter and the Philosophers Stone",
+          author: "J.K Rowling",
+          pages: 368,
+          readStatus: true,
+        },
+        {
+          title: "Eragon",
+          author: "Christopher",
+          pages: 250,
+          readStatus: false,
+        },
+      ],
+    };
+  }
 
   render() {
-
-        return (
+    const myLibrary = this.state.library.map((book) => {
+      return(
         <div>
-
-        </div>
-        )
+        <h3>{book.title}</h3>
+        <p>{book.author}</p>
+        <p>{book.pages}</p>
+      </div>
+      );
     });
 
     return (
       <div>
-        
+        {myLibrary}
       </div>
-    );
+    )
   }
 }
 
