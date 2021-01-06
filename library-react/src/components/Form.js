bce4699d201a2c3180affd3ff9dcfb143ebb2656
Jaime Ortiz <jxme88@gmail.com>
2021-01-05 19:06:52 -0800

    Added input and sets the state with value

--- library-react/src/components/Form.js
+++ library-react/src/components/Form.js
@@ -6,25 +6,59 @@ class Form extends Component {
     this.state = {
       title: '',
       author: '',
-      pages: '',
+      pages: 0,
       readStatus: false
     }
 
   }
 
+ handleChangeInput= (e) => {
+ 
+  this.setState({
+    [e.target.name]: e.target.value
+  })
+  console.log(this.state);
+}
 
+handleSubmit = (e) => {
+ e.preventDefault();
+
+console.log(this.state)
+}
 
   render() {
     return (
       <div>
         <form>
-          <input 
-          type='text'
-          className='input'
-          placeholder='Enter Title'
-          value={this.state.title}
-          onChange={(e) => this.setState({title: e.target.value})}
+          <input
+            type="text"
+            className="input"
+            placeholder="Enter Title"
+            name="title"
+            value={this.state.title}
+            onChange={this.handleChangeInput}
+          /> <br/>
+          <input
+            type="text"
+            className="input"
+            placeholder="Author"
+            name="author"
+            value={this.state.author}
+            onChange={this.handleChangeInput}
+          /> <br/>
+          <input
+            type="number"
+            className="input"
+            placeholder="Pages"
+            name="pages"
+            value={this.state.pages}
+            onChange={this.handleChangeInput}
           />
+        <button
+          onClick={this.handleSubmit.bind(this)}
+        >
+          Submit
+        </button>
         </form>
       </div>
     );
