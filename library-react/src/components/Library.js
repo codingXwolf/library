ce1e18c2a3cdfdd68b8811e8322783193f51d85d
Jaime Ortiz <jxme88@gmail.com>
2021-01-23 23:54:57 -0800

    Changed key name to data to store items in local storage, only temporary. When component renders local storage displays items even after refrshing.

--- library-react/src/components/Library.js
+++ library-react/src/components/Library.js
@@ -8,36 +8,40 @@ class Library extends Component {
   constructor(props) {
     super(props);
 
+    let localStorageBookData = JSON.parse(localStorage.getItem('data'));
+    console.log(localStorageBookData);
+
     this.state = {
       library: [
-        {
-          id: 1,
-          title: "Harry Potter and the Philosophers Stone",
-          author: "J.K Rowling",
-          pages: 368,
-          readStatus: true,
-        },
-        {
-          id: 2,
-          title: "Eragon",
-          author: "Christopher",
-          pages: 250,
-          readStatus: false,
-        },
-        {
-          id: 3,
-          title: "Hell Divers",
-          author: "Nicholas Sansbury Smith",
-          pages: 431,
-          readStatus: true,
-        },
-        {
-          id: 4,
-          title: "The Stand",
-          author: "Stephen King",
-          pages: 1172,
-          readStatus: null,
-        },
+        localStorageBookData
+        // {
+        //   id: 1,
+        //   title: "Harry Potter and the Philosophers Stone",
+        //   author: "J.K Rowling",
+        //   pages: 368,
+        //   readStatus: true,
+        // },
+        // {
+        //   id: 2,
+        //   title: "Eragon",
+        //   author: "Christopher",
+        //   pages: 250,
+        //   readStatus: false,
+        // },
+        // {
+        //   id: 3,
+        //   title: "Hell Divers",
+        //   author: "Nicholas Sansbury Smith",
+        //   pages: 431,
+        //   readStatus: true,
+        // },
+        // {
+        //   id: 4,
+        //   title: "The Stand",
+        //   author: "Stephen King",
+        //   pages: 1172,
+        //   readStatus: null,
+        // },
       ],
     };
   }
@@ -53,11 +57,11 @@ class Library extends Component {
       pages: book.pages.value,
       readStatus: convertReadStatus,
     };
-        
+
     this.setState({ library: [...this.state.library, newBook] });
 
-    localStorage.setItem(newBook.id, JSON.stringify(newBook));
-    console.log(JSON.parse(localStorage.getItem(`${newBook.id}`)))
+    localStorage.setItem('data', JSON.stringify(newBook));
+    console.log(JSON.parse(localStorage.getItem(`${newBook.id}`)));
   };
 
   delBook = (book) => {
@@ -65,13 +69,11 @@ class Library extends Component {
     this.setState({ library });
   };
 
-
-
-  render() {
+  render() {    
     const myLibrary = this.state.library.map((book) => {
       return (
         <React.Fragment key={book.id}>
-            <BookCard data={book} delBook={this.delBook}></BookCard>
+          <BookCard data={book} delBook={this.delBook}></BookCard>
         </React.Fragment>
       );
     });
@@ -90,7 +92,9 @@ class Library extends Component {
         <BookForm addBook={this.addBook} />
         <br />
         <br />
-          <CardGroup centered doubling>{myLibrary}</CardGroup>
+        <CardGroup centered doubling>
+          {myLibrary}
+        </CardGroup>
       </React.Fragment>
     );
   }
